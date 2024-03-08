f1fa489dad0eb22da3df5572e43eab1dc8875003
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-03-08 16:17:05 +0530

    new multiple pointers pattern

---
+++ multiplePointers/sumZero.js
@@ -0,0 +1,16 @@
+function sumZero(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === 0) {
+      return [arr[left], arr[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+}
+const arr = [-4, -3, -2, -1, 0, 1, 2, 3, 10];
+console.log(sumZero(arr));
